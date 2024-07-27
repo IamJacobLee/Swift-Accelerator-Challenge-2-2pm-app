31cbcbafc715ecbad91bb026f65a59e6265f184e
Kuroneko188 <2011078069@qq.com>
2024-07-27 14:15:17 +0800

    add content view

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -1,1 +1,18 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by Li Xianlin on 27/7/24.
+//
 
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
