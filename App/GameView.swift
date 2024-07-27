08f128fb0233a9e3341ec5782b8447ece8e3c48e
Kuroneko188 <2011078069@qq.com>
2024-07-27 15:30:14 +0800

    Heeheeheeheehaha

--- App/GameView.swift
+++ App/GameView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct GameView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            Text("Pick up the recyclable items!")
+                .navigationTitle("Game")
+        }
     }
+    
 }
 
 #Preview {
