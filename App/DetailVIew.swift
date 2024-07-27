0996c5eb9c6f2b7c7cb6e41c13994990e2e2a0a1
Kuroneko188 <2011078069@qq.com>
2024-07-27 14:31:25 +0800

    Added all views

---
+++ App/DetailVIew.swift
@@ -0,0 +1,18 @@
+//
+//  DetailVIew.swift
+//  App
+//
+//  Created by Li Xianlin on 27/7/24.
+//
+
+import SwiftUI
+
+struct DetailVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailVIew()
+}
