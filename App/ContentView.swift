1421a2401b46e0b6146e11ae4e37dbaf0d80d17a
Kuroneko188 <2011078069@qq.com>
2024-07-27 15:09:34 +0800

    Added tab bars

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -9,10 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ScannerView()
+                .tabItem {
+                    Label("Scanner", systemImage: "qrcode.viewfinder")
+                }
+            
+            GameView()
+                .tabItem {
+                    Label("Game", systemImage: "gamecontroller")
+                }
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
+
+
--- App/ScannerView.swift
+++ App/ScannerView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ScannerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            
+            Text("Tap the dot to see information")
+            
+                .navigationTitle("Scanner")
+            
+        }
     }
 }
 
