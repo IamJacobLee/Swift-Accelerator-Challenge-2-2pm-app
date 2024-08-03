18ddad00ed8523a5dff9815cfb83b681a301e77e
Jacob Lee <jacob.lee.jiege@sji.edu.sg>
2024-08-03 16:01:55 +0800

    Scanning update

---
+++ App/ScannerConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  ScannerConfiguration.swift
+//  App
+//
+//  Created by J Family on 3/8/24.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ViewController
+    
+
+}
