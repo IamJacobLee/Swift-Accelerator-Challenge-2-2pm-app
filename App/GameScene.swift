5ccee0915b1ff09a47bb835fd41285372c202778
Christine <christinelee263@gmail.com>
2024-08-03 11:46:20 +0800

    hahahaha hahaha hahah ha

---
+++ App/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  App
+//
+//  Created by T Krobot on 3/8/24.
+//
+
+import SwiftUI
+
+struct GameScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameScene()
+}
---
+++ App/GameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewController.swift
+//  App
+//
+//  Created by T Krobot on 3/8/24.
+//
+
+import SwiftUI
+
+struct GameViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameViewController()
+}
